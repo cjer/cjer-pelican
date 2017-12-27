b518d688f6cc540010dfabe021b6b9fc5d479ed5
cjer <9987721+cjer@users.noreply.github.com>
2017-12-27 18:01:12 +0200

    - fix blogroll and social shit for real

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,12 +23,10 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Social link will come here', '#'),)
 
 DEFAULT_PAGINATION = 10
 
