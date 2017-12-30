5cb40953ca097cafc58062b6caaea412f10b5ff9
cjer <9987721+cjer@users.noreply.github.com>
2017-12-30 16:58:28 +0200

    - add disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ import os
 
 AUTHOR = 'Dan Bareket'
 SITENAME = 'associlate'
-SITEURL = ''
+SITEURL = 'https://cjer.github.io'
 
 PATH = 'content'
 
@@ -14,12 +14,16 @@ TIMEZONE = 'Asia/Tel_Aviv'
 DEFAULT_LANG = 'English'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+DELETE_OUTPUT_DIRECTORY = True
+
 GOOGLE_ANALYTICS = "UA-111620097-1"
+DISQUS_SITENAME = "cjer"
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
