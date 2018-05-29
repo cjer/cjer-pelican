c152a31f907320d7a748b60720a98e886cb40ae7
cjer <9987721+cjer@users.noreply.github.com>
2018-05-30 02:41:52 +0300

    upload new notebook

---
+++ content/AutoTel - Initial Analysis.md
@@ -0,0 +1,9 @@
+Title: AutoTel Unboxing
+Date: 2018-05-28 13:15
+Modified: 2018-05-30 02:36
+Category: Posts
+Tags: autotel, geo, pandas, seaborn, geopandas, shapely, bokeh, holoviews, pyviz, datashader, altair
+Authors: Dan Bareket
+Summary: Analyzing AutoTel trip data.
+
+{% notebook autotel_01_initial_analysis.ipynb %}
--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,7 @@ EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
     'extra/tarde_monadology.pdf': {'path': 'tarde_monadology.pdf'},
     'extra/tsne_30_topmk.html': {'path': 'tsne_30_topmk.html'},
+    'extra/twitter_heatmap.html': {'path': 'twitter_heatmap.html'},
     # 'extra/google7a5dd5ba97352752.html': {'path': 'google7a5dd5ba97352752.html'},
     # 'extra/BingSiteAuth.xml': {'path': '/BingSiteAuth.xml'},
     # 'extra/yandex_0535b382efa4270d.html': {'path': '/yandex_0535b382efa4270d.html'},
